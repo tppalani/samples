4e74ad9ca483c32b60b66c5bfb76f2558e823027
tppalani <palani160385@gmail.com>
2023-10-02 15:46:45 +0530

    Update server.js

--- nodejs/npm/server.js
+++ nodejs/npm/server.js
@@ -5,13 +5,32 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send(`<!DOCTYPE html>
-<html>
-  <head>
-    <title>Powered By Paketo Buildpacks</title>
-  </head>
-  <body>
-    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Buildpacks.io Java Sample</title>
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <style>
+        html, body {
+            font-family: sans-serif;
+            margin: 0;
+            height: 100%;
+        }
+        .content-centered {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="content-centered" style="margin: 0 auto; flex-direction: column; width: 67%; height: 100%;">
+        <div class="content-centered">
+            <a href="https://buildpacks.io"><img src="https://buildpacks.io/docs/concepts/how.svg" alt="Buildpacks.io Logo" style="width:50vw;"></a>
+        </div>
+        <h1>Hello, Buildpacker first Release!</h1>
+    </div>
+</body>
 </html>`);
 });
 
